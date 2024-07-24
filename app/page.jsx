029c46515887b898b4c0a044eee57588c99b80fe
Sterling Newsom <sterlingnewsom808@gmail.com>
2024-07-24 15:26:28 -0400

    made some changes to UI again

--- app/page.jsx
+++ app/page.jsx
@@ -9,11 +9,11 @@ const page = () => {
     <div className="content-wrapper text-center mt-5">
       <div className="font-bold text-7xl text-wrap purple_gradient text-center">Welcome Alexi!</div>
       <p className="text-s  font-light  text-center">Your personalized Learning Journey awaits!</p>
-      <Button radius="full" className ="bg-gradient-to-tr from-purple-500 to-orange-100 text-white shadow-lg px-10 py-3 mt-5 cursor-pointer">
+      <Button radius="full" className ="bg-gradient-to-tr from-purple-700 to-orange-100 text-white shadow-lg px-10 py-3 mt-5 cursor-pointer">
         Explore!
       </Button>
       <Image
-          src="/Images/home-page-3d-img.png"
+          src="/Images/portrait-young-student-with-book-education-day.png" 
           width={800}
           height={800}
           alt="3d image of black woman sitting on couch"

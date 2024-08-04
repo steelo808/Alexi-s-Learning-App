0cb521b247a34134a1e115cf7d94a5d13157040a
Sterling Newsom <sterlingnewsom808@gmail.com>
2024-08-04 00:25:53 -0400

    made some changes to ui

--- app/math/page.jsx
+++ app/math/page.jsx
@@ -1,19 +1,29 @@
 "use client"
 import React from 'react';
 import {useState} from 'react';
-import {Button} from "@nextui-org/button";
+import {Button, ButtonGroup} from "@nextui-org/button";
 import { Progress } from '@nextui-org/react';
+import Image from 'next/image';
 
 
 const Math = () => {
   return (
     <div className="wrapper text-center">
+    <ButtonGroup className="flex flex-wrap gap-4 items-center justify-center">
     <Button radius="full" variant="shadow" className="bg-gradient-to-tr from-purple-700 to-orange-100 text-white shadow-lg px-10 py-3 mt-5 cursor-pointer text-m font-bold">Start Timer</Button>
     <Button color ="white" radius="full" variant="shadow" className="bg-gradient-to-tr from-purple-700 to-orange-100 text-white shadow-lg px-10 py-3 mt-5 cursor-pointer text-m font-bold">Stop Timer</Button>
     <Button radius="full" variant="shadow" className="bg-gradient-to-tr from-purple-700 to-orange-100 text-white shadow-lg px-10 py-3 mt-5 cursor-pointer text-m font-bold">Clear Timer</Button>
-    <div className ="timer">00</div>
-    <Progress
-        aria-label = "keep going! you got this!"
+    </ButtonGroup>
+    <div className ="timer text-xl font-bold py-3">00:00</div>
+    <Image
+      src="/Images/ai-girl.png"
+      width={400}
+      height={400}
+      alt="3d image of black girl"
+      className="ai-girl"
+    />
+    {/* <Progress
+        aria-label ="keep going! you got this!"
         size="lg"
         value={50}
         color="purple"
@@ -25,7 +35,7 @@ const Math = () => {
             label: "tracking-wider font-medium text-default-600",
             value:"text-foreground/50"
         }}
-    />
+    /> */}
 
     </div>
   );
